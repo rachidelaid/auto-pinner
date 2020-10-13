1cc17a54d4bcea2fc869f79d0f531a0dc559b24d
rachid <rachidelaid@gmail.com>
2020-10-13 21:53:57 +0100

    first commit

--- main.js
+++ main.js
@@ -1,43 +1,295 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
-const path = require('path')
+const { app, BrowserWindow, ipcMain, Notification } = require("electron");
+const fetch = require("node-fetch");
+const fs = require("fs");
 
-function createWindow () {
+const download = require("image-downloader");
+
+const low = require("lowdb");
+const FileSync = require("lowdb/adapters/FileSync");
+
+const pie = require("puppeteer-in-electron");
+const puppeteer = require("puppeteer-core");
+(async () => {
+  await pie.initialize(app);
+})();
+
+const adapter = new FileSync("db.json");
+const db = low(adapter);
+db.defaults({ data: [] }).write();
+
+let mainWindow;
+function createWindow() {
   // Create the browser window.
-  const mainWindow = new BrowserWindow({
+  mainWindow = new BrowserWindow({
     width: 800,
-    height: 600,
+    height: 650,
+    resizable: false,
+    resizable: false,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
-    }
-  })
+      // preload: path.join(__dirname, 'preload.js')
+      nodeIntegration: true,
+    },
+  });
 
-  // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
-
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+  mainWindow.loadFile("./page/index.html");
+  mainWindow.on("close", function (event) {
+    app.quit();
+  });
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
+ipcMain.on("post-pin", async (e, arg) => {
+  if (!fs.existsSync("../images")) {
+    fs.mkdirSync("../images");
+  }
+
+  const browser = await pie.connect(app, puppeteer);
+
+  const win = new BrowserWindow({
+    width: 800,
+    height: 650,
+    show: arg.hidden == "false" ? true : false,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+  if (arg.hidden == "false") {
+    win.maximize();
+  }
+
+  const ses = win.webContents.session;
+  await ses.clearAuthCache();
+  await ses.clearCache();
+  await ses.clearStorageData();
+
+  try {
+    const page = await pie.getPage(browser, win);
+
+    mainWindow.webContents.send("update", `opening pinterest`);
+    try {
+      await page.goto("https://pinterest.com/", {
+        timeout: 0,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+    ///////////////////////////////////////////////////////////////////////
+    mainWindow.webContents.send("update", `login to ${arg.username}`);
+    await page.waitForSelector('[data-test-id="simple-login-button"] button');
+    console.log("not logged in");
+    await page.evaluate(() => {
+      document
+        .querySelector('[data-test-id="simple-login-button"] button')
+        .click();
+    });
+
+    await page.waitForSelector("button[type='submit']");
+    await page.waitFor(1000);
+
+    await page.evaluate(() => {
+      if (document.querySelector('[data-test-id="login-switch-account"] a')) {
+        document
+          .querySelector('[data-test-id="login-switch-account"] a')
+          .click();
+      }
+    });
+
+    await page.waitFor(1000);
+
+    let userInput = await page.waitForSelector("input#email");
+    await userInput.type(arg.username, { delay: 100 });
+
+    let passInput = await page.waitForSelector("input#password");
+    await passInput.type(arg.pass, { delay: 100 });
+
+    await page.waitFor(1000);
+
+    await page.evaluate(() => {
+      document.querySelector("button[type='submit']").click();
+    });
+
+    await page.waitForSelector(
+      '[data-test-id="header-accounts-options-button"] button',
+      {
+        timeout: 0,
+      }
+    );
+    ////////////////////////////////////////////////////////////////////////////////
+    for (let j = 0; j < arg.data.length; j++) {
+      try {
+        const list = arg.data[j];
+
+        mainWindow.webContents.send("update", `creating a pin`);
+        await page.goto("https://www.pinterest.com/pin-builder/", {
+          timeout: 0,
+        });
+        console.log("page fully loaded");
+        await page.waitForSelector(
+          'button[data-test-id="board-dropdown-select-button"]',
+          {
+            timeout: 0,
+          }
+        );
+        await page.waitFor(1000);
+        await page.evaluate(() => {
+          const loop = setInterval(() => {
+            if (
+              document.querySelector(
+                'button[data-test-id="board-dropdown-select-button"]'
+              )
+            ) {
+              document
+                .querySelector(
+                  'button[data-test-id="board-dropdown-select-button"]'
+                )
+                .click();
+
+              clearInterval(loop);
+            }
+          }, 200);
+        });
+
+        const bd = `[data-test-id="boardWithoutSection"] [title="${arg.board}"]`;
+        await page.waitForSelector(bd, {
+          timeout: 0,
+        });
+        await page.waitFor(1000);
+        await page.evaluate((bd) => {
+          const loop = setInterval(() => {
+            if (document.querySelector(bd)) {
+              document
+                .querySelector(bd)
+                .parentElement.parentElement.parentElement.click();
+
+              clearInterval(loop);
+            }
+          }, 200);
+        }, bd);
+
+        mainWindow.webContents.send("update", `uploading the image`);
+        await download.image({
+          url: list.img,
+          dest: `../images/image-${j}.${
+            list.img.split(".")[list.img.split(".").length - 1]
+          }`,
+        });
+
+        const [fileChooser] = await Promise.all([
+          page.waitForFileChooser(),
+          page.click("#media-upload-input"),
+        ]);
+        await fileChooser.accept([
+          `../images/image-${j}.${
+            list.img.split(".")[list.img.split(".").length - 1]
+          }`,
+        ]);
+
+        let titleInput = await page.waitForSelector(
+          'textarea[id^="pin-draft-title"]'
+        );
+        await page.waitFor(1000);
+        mainWindow.webContents.send("update", `typing the title`);
+        await titleInput.type(list.title.substring(0, 100), { delay: 10 });
+        await page.keyboard.press("Tab");
+
+        await page.waitFor(500);
+        mainWindow.webContents.send("update", `typing the description`);
+        const description = list.newDesc
+          ? list.newDesc
+          : list.desc.substring(0, 500);
+
+        const newString = description.replace(
+          /\b((?:[a-z][\w-]+:(?:\/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}\/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’]))/g,
+          ""
+        );
+        await page.keyboard.type(newString, { delay: 10 });
+
+        if (list.link.includes('amzn.to')) {
+          const resp = await fetch("https://selffast.com/url", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              url: list.link,
+            }),
+          });
+
+          const { name: shortID } = await resp.json();
+
+          await page.keyboard.press("Tab");
+          await page.waitFor(500);
+          mainWindow.webContents.send("update", `entering the url`);
+          await page.keyboard.type(`https://www.selffast.com/${shortID}`, {
+            delay: 50,
+          });
+        } else {
+          const resp = await fetch("https://selffast.com/url", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              url: `https://track.fiverr.com/visit/?bta=${arg.affiliateID}&brand=fiverrcpa&landingPage=${list.link}`,
+            }),
+          });
+
+          const { name: shortID } = await resp.json();
+
+          await page.keyboard.press("Tab");
+          await page.waitFor(500);
+          mainWindow.webContents.send("update", `entering the url`);
+          await page.keyboard.type(`https://www.selffast.com/${shortID}`, {
+            delay: 50,
+          });
+        }
+
+        await page.evaluate(() => {
+          document
+            .querySelector('button[data-test-id="board-dropdown-save-button"]')
+            .click();
+        });
+
+        try {
+          await page.waitForSelector('svg[aria-label="Saving Pin..."]');
+          await page.waitForFunction(
+            () => !document.querySelector('svg[aria-label="Saving Pin..."]')
+          );
+
+          const sec = parseInt(arg.delay) + Math.floor(Math.random() * 10) + 1;
+          mainWindow.webContents.send("update", `waiting ${sec} seconds`);
+          mainWindow.webContents.send("progress", list.link);
+          await page.waitFor(sec * 1000);
+        } catch (err) {
+          mainWindow.webContents.send("error", err);
+        }
+      } catch (error) {
+        mainWindow.webContents.send("error", error);
+      }
+    }
+  } catch (error) {
+    console.log(error);
+  }
+
+  mainWindow.webContents.send("finished");
+  win.close();
+
+  const myNotification = new Notification({
+    title: "Auto Pinner",
+    body: "the task is finished",
+    icon: "./page/icon.png",
+  });
+  myNotification.show();
+});
+
 app.whenReady().then(() => {
-  createWindow()
-  
-  app.on('activate', function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
-})
+  createWindow();
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
-app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') app.quit()
-})
+  app.on("activate", function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+app.on("window-all-closed", function () {
+  if (process.platform !== "darwin") app.quit();
+});
--- renderer.js
+++ renderer.js
@@ -1,6 +1,236 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// No Node.js APIs are available in this process because
-// `nodeIntegration` is turned off. Use `preload.js` to
-// selectively enable features needed in the rendering
-// process.
+const { ipcRenderer } = require("electron");
+const low = require("lowdb");
+const FileSync = require("lowdb/adapters/FileSync");
+
+const adapter = new FileSync("db.json");
+const db = low(adapter);
+db.defaults({ data: [], settings: {} }).write();
+
+new Vue({
+  el: "#app",
+  data() {
+    return {
+      pinPassword: null,
+      pinUsername: null,
+      affiliateID: null,
+      delay: 30,
+      accounts: [],
+      lists: [],
+      currentItem: null,
+      oldDesc: "",
+      progress: 0,
+      progressSVG: 350,
+      boardUrl: "",
+      doneL: 0,
+      count: 0,
+    };
+  },
+  methods: {
+    addAccount() {
+      if (this.pinUsername != null && this.pinPassword != null) {
+        const obj = {
+          pass: this.pinPassword,
+          user: this.pinUsername,
+        };
+
+        this.accounts.push(obj);
+
+        db.set("settings.accounts", JSON.stringify(this.accounts)).write();
+
+        //localStorage.setItem("accounts", JSON.stringify(this.accounts));
+
+        this.pinPassword = null;
+        this.pinUsername = null;
+      }
+    },
+    closeSetting() {
+      document.querySelector("#setting").style.display = "none";
+      document.querySelector("#home").style.display = "block";
+    },
+    save() {
+      if (this.affiliateID != null) {
+        //localStorage.setItem("affiliateID", this.affiliateID);
+        db.set("settings.affiliateID", this.affiliateID).write();
+      }
+
+      if (this.delay != null) {
+        //localStorage.setItem("pinDelay", this.delay);
+        db.set("settings.pinDelay", this.delay).write();
+      }
+
+      const acc = document.querySelector("#accounts").value;
+      //localStorage.setItem("selectedACC", acc);
+      db.set("settings.selectedACC", acc).write();
+
+      const hide = document.querySelector("#hidden").value;
+      //localStorage.setItem("hidden", hide);
+      db.set("settings.hidden", hide).write();
+
+      document.querySelector("#setting").style.display = "none";
+      document.querySelector("#home").style.display = "block";
+    },
+    showSetting() {
+      document.querySelector("#home").style.display = "none";
+      document.querySelector("#setting").style.display = "block";
+    },
+    loadJSON() {
+      document.querySelector(".confirmJSON").style.display = "flex";
+    },
+    cancelJSON() {
+      document.querySelector(".confirmJSON").style.display = "none";
+    },
+    replaceJSON() {
+      document.querySelector("#jsonInput").click();
+      document.querySelector(".confirmJSON").style.display = "none";
+
+      document.querySelector("#jsonInput").addEventListener("change", (e) => {
+        const data = require(e.target.files[0].path);
+
+        db.set("data", [...data]).write();
+
+        this.lists = [];
+        this.lists = db.get("data").value();
+      });
+    },
+    appendJSON() {
+      document.querySelector("#jsonInput").click();
+      document.querySelector(".confirmJSON").style.display = "none";
+
+      document.querySelector("#jsonInput").addEventListener("change", (e) => {
+        const data = require(e.target.files[0].path);
+        
+        db.get("data").push(...data).write();
+
+        this.lists = [];
+        this.lists = db.get("data").value();
+      });
+    },
+    edit(list, i) {
+      this.currentItem = { ...list };
+      this.currentItem.id = i;
+
+      this.oldDesc = list.desc;
+      document.querySelector(".editItem").style.display = "flex";
+    },
+    closeEdit() {
+      document.querySelector(".editItem").style.display = "none";
+      this.lists = [];
+      this.lists = db.get("data").value();
+    },
+    deleteItem(link) {
+      document.querySelector(".editItem").style.display = "none";
+      db.get("data").remove({ link }).write();
+
+      this.lists = [];
+      this.lists = db.get("data").value();
+    },
+    saveItem() {
+      document.querySelector(".editItem").style.display = "none";
+
+      let id = this.currentItem.id;
+      delete this.currentItem.id;
+
+      this.lists[id] = this.currentItem;
+
+      db.set("data", [...this.lists]).write();
+
+      this.lists = [];
+      this.lists = db.get("data").value();
+    },
+    post() {
+      document.querySelector(".beforePosting").style.display = "flex";
+      document.querySelector("input#board").focus();
+    },
+    unpost() {
+      document.querySelector(".beforePosting").style.display = "none";
+    },
+    start() {
+      if (this.boardUrl.trim() != "") {
+        document.querySelector(".beforePosting").style.display = "none";
+        document.querySelector(".posting").style.display = "flex";
+
+        let username;
+        let pass;
+
+        for (let i = 0; i < this.accounts.length; i++) {
+          const element = this.accounts[i];
+          if (document.querySelector("#accounts").value === element.user) {
+            username = element.user;
+            pass = element.pass;
+          }
+        }
+
+        this.count = this.lists.length;
+
+        ipcRenderer.send("post-pin", {
+          hidden: document.querySelector("#hidden").value,
+          affiliateID: this.affiliateID,
+          data: this.lists,
+          board: this.boardUrl,
+          delay: this.delay,
+          username,
+          pass,
+        });
+      }
+    },
+  },
+  mounted() {
+    const data = db.get("data").value();
+    if (data.length > 0) {
+      this.lists = data;
+    }
+
+    const settings = db.get("settings").value();
+
+    if (settings.accounts) {
+      this.accounts = [...JSON.parse(settings.accounts)];
+    }
+
+    if (settings.affiliateID) {
+      this.affiliateID = settings.affiliateID;
+    }
+
+    if (settings.pinDelay) {
+      this.delay = settings.pinDelay;
+    }
+
+    if (settings.selectedACC) {
+      setTimeout(() => {
+        document.querySelector("#accounts").value = settings.selectedACC;
+      }, 100);
+    }
+
+    if (settings.hidden) {
+      setTimeout(() => {
+        document.querySelector("#hidden").value = settings.hidden;
+      }, 100);
+    }
+
+    ipcRenderer.on("progress", (event, link) => {
+      db.get("data").remove({ link }).write();
+      this.lists = [];
+      this.lists = db.get("data").value();
+
+      this.progressSVG -= 350 / this.count;
+      this.doneL++;
+
+      if (this.progressSVG <= 0) {
+        this.progress = 100;
+      } else {
+        this.progress += parseInt(100 / this.count);
+      }
+    });
+
+    ipcRenderer.on("update", (event, update) => {
+      document.querySelector(".posting label").innerText = update;
+    });
+
+    ipcRenderer.on("error", (event, error) => {
+      console.log(error);
+    });
+
+    ipcRenderer.on("finished", (event, error) => {
+      document.querySelector(".posting").style.display = "none";
+    });
+  },
+});
